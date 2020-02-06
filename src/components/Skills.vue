7b6b168eaad0d51475e2c07bf10fb80d3b6d1eb5
Carlos Diaz <cdiaz86@gmail.com>
2020-02-05 21:25:57 -0500

    Adds multiple classes to div with v-binding.

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,32 +1,43 @@
 <template>
   <div class="hello">
-    Skills
+    <div class="holder">
+      <ul>
+        <li v-for="(data, index) in skills" :key='index'>{{ index + 1 }}. {{ data.skill }}</li>
+      </ul>
+
+      <div v-bind:class="alertObject"></div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Skills',
-  props: {
-    
+  data() {
+    return {
+      skills: [
+        { 'skill': 'Vue.js'},
+        { 'skill': 'Frontend Developer'},
+        { 'skill': 'JavaScript'},
+        { 'skill': 'Ruby'},
+      ],
+      alertObject: {
+        alert: true,
+        anotherClass: true,
+      }
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .alert {
+    background-color: yellow;
+    width: 100%;
+    height: 30px;
+  }
+
+  .anotherClass {
+    border: 5px solid black;
+  }  
 </style>
